53c0db9a4abe64f900e7920dcc43aae5b9fe333d
hyalis <hyalis81@gmail.com>
2014-04-01 11:05:19 +0200

    Delete des fichiers sur GIT

--- Projet/Backup/include/bdd.php
+++
@@ -1,7 +0,0 @@
-<?php
-	// Connection au serveur
-	$dns = 'mysql:host=db515321521.db.1and1.com;dbname=db515321521';
-	$utilisateur = 'dbo515321521';
-	$motDePasse = 'onlyforme';
-	$connection = new PDO( $dns, $utilisateur, $motDePasse );
-?>
